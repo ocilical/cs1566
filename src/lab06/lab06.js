e9f56546ab7f1c9c70a83ce39bf4979589546f11
ocilical <32717754+ocilical@users.noreply.github.com>
2022-10-27 11:24:47 -0400

    do lab 6

---
+++ src/lab06/lab06.js
@@ -0,0 +1,145 @@
+"use strict";
+var Lab06;
+(function (Lab06) {
+    // These variables must be global variables.
+    // Some callback functions may need to access them.
+    let gl;
+    let canvas;
+    let ctm_location;
+    let model_view_location;
+    let projection_location;
+    let identity = [
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0]
+    ];
+    let ctm = [
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0],
+    ];
+    let projection = Camera.ortho(-2, 2, -2, 2, 2, -10);
+    let model_view = Camera.lookAt([0, 1, 1, 1], [0, 0, 0, 1], [0, 1, 0, 0]);
+    let isAnimating = true;
+    let animTime = 0;
+    let positions;
+    function initGL(canvas) {
+        gl = canvas.getContext("webgl");
+        if (!gl) {
+            alert("WebGL is not available...");
+            return -1;
+        }
+        // Set the clear screen color to black (R, G, B, A)
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        // Enable hidden surface removal
+        gl.enable(gl.DEPTH_TEST);
+        gl.enable(gl.CULL_FACE);
+        return 0;
+    }
+    function init() {
+        if (!gl)
+            return -1;
+        positions = Mesh.cube();
+        let colors = Mesh.cubeAxisColors();
+        // Load and compile shader programs
+        let shaderProgram = initShaders(gl, "vertex-shader", "fragment-shader");
+        if (shaderProgram === -1)
+            return -1;
+        gl.useProgram(shaderProgram);
+        // Allocate memory in a graphics card
+        let buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, 4 * 4 * (positions.length + colors.length), gl.STATIC_DRAW);
+        // Transfer positions and put it at the beginning of the buffer
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, to1DF32Array(positions));
+        // Transfer colors and put it right after positions
+        gl.bufferSubData(gl.ARRAY_BUFFER, 4 * 4 * positions.length, to1DF32Array(colors));
+        // Vertex Position - locate and enable "vPosition"
+        let vPosition_location = gl.getAttribLocation(shaderProgram, "vPosition");
+        if (vPosition_location === -1) {
+            alert("Unable to locate vPosition");
+            return -1;
+        }
+        gl.enableVertexAttribArray(vPosition_location);
+        // vPosition starts at offset 0
+        gl.vertexAttribPointer(vPosition_location, 4, gl.FLOAT, false, 0, 0);
+        // Vertex Color - locate and enable vColor
+        let vColor_location = gl.getAttribLocation(shaderProgram, "vColor");
+        if (vColor_location === -1) {
+            alert("Unable to locate vColor");
+            return -1;
+        }
+        gl.enableVertexAttribArray(vColor_location);
+        // vColor starts at the end of positions
+        gl.vertexAttribPointer(vColor_location, 4, gl.FLOAT, false, 0, 4 * 4 * positions.length);
+        // Current Transformation Matrix - locate and enable "ctm"
+        ctm_location = gl.getUniformLocation(shaderProgram, "ctm");
+        if (ctm_location === null) {
+            alert("Unable to locate ctm");
+            return -1;
+        }
+        model_view_location = gl.getUniformLocation(shaderProgram, "model_view");
+        if (model_view_location === null) {
+            alert("Unable to locate model_view");
+            return -1;
+        }
+        projection_location = gl.getUniformLocation(shaderProgram, "projection");
+        if (projection_location === null) {
+            alert("Unable to locate projection");
+            return -1;
+        }
+        return 0;
+    }
+    function display() {
+        if (!gl) {
+            console.log("WebGL not initialized");
+            return;
+        }
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.uniformMatrix4fv(ctm_location, false, to1DF32Array(ctm));
+        gl.uniformMatrix4fv(model_view_location, false, to1DF32Array(model_view));
+        gl.uniformMatrix4fv(projection_location, false, to1DF32Array(projection));
+        gl.drawArrays(gl.TRIANGLES, 0, positions.length);
+    }
+    function idle() {
+        // Calculate camera position
+        let eyepos = [
+            Math.sin(animTime / 20),
+            Math.cos(animTime / 20),
+            Math.cos(animTime / 20),
+            1,
+        ];
+        model_view = Camera.lookAt(eyepos, [0, 0, 0, 1], [0, 1, 0, 0]);
+        animTime += 1;
+        // Draw
+        display();
+        if (isAnimating === true)
+            requestAnimationFrame(idle);
+    }
+    // This function will be called when a keyboard is pressed.
+    function keyDownCallback(event) {
+        console.log("keyDownCallback(): " +
+            "event.key = " + event.key);
+        if (event.key === " ") {
+            isAnimating = !isAnimating;
+            if (isAnimating)
+                requestAnimationFrame(idle);
+        }
+    }
+    function main() {
+        canvas = document.getElementById("gl-canvas");
+        if (initGL(canvas) === -1)
+            return -1;
+        if (init() === -1)
+            return -1;
+        // Register callback functions
+        // Comment out those that are not used.
+        document.addEventListener("keydown", keyDownCallback);
+        display();
+        if (isAnimating)
+            requestAnimationFrame(idle);
+    }
+    Lab06.main = main;
+})(Lab06 || (Lab06 = {}));
